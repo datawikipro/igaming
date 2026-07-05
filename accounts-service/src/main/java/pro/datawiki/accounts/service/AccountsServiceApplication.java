75587f32d38d7beb3d2644f89a6188e2508945f9
Aleksei Chernousov <aleksei.a.chernousov@gmail.com>
2026-07-06 00:50:53 +0300

    Fix Spring JPA EntityScan

--- accounts-service/src/main/java/pro/datawiki/accounts/service/AccountsServiceApplication.java
+++ accounts-service/src/main/java/pro/datawiki/accounts/service/AccountsServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = {"pro.datawiki.accounts"})
+@EntityScan(basePackages = {"pro.datawiki.accounts.domain.entity"})
 @EnableScheduling
 public class AccountsServiceApplication {
 
